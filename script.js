1e70496e27023224e73764c90e289f697f536611
Denis <75341375+Dcatnis@users.noreply.github.com>
2021-03-28 20:40:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function FunctionPopup(){
+  var popup=document.getElementById("Popup")
+  var iconShare=document.getElementById("shareIcon")
+  var Michelle=document.getElementById("michelle")
+  popup.classList.toggle("show") 
+  iconShare.classList.toggle("colorIcon")
+  Michelle.classList.toggle("hiddenM")
+  iconShare.classList.toggle("shareMobile")
+}
+
